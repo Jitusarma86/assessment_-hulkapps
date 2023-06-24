ff499158844970464c2ba12dfd5232e967daa5df
jitusarma86 <jitusarma86@gmail.com>
2023-06-24 11:22:44 +0530

    Commited

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,9 +4,43 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
     <title>Admin</title>
 </head>
 <body>
-    Admin
+<div class="container">
+            <!-- About wrapper -->
+            <div class="about-wrapper">
+                <div class="content" style="height:500px;">
+                <br>
+                <div class="row">
+                        <div class="col-lg-8 col-md-12 col-sm-12">
+                        <form id="fileUploadForm" action="{{url('/')}}/home" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <input type="file" name="mycsv" id="mycsv" class="form-control" value="">
+                        
+                        
+                        <div id="pbar" style="display:none">        
+                        <p class="text-success">Note: Don't refresh the page, file is uploading.... </p>
+                        </div>
+                        </div>
+                        <div class="col-lg-4 col-md-12 col-sm-12">  
+                        <button class="btn btn-primary" name="upload" id="upload" >Import</button>  
+
+                        
+                        <div id="spenner" style="display:none;">
+                            <div class="spinner-border text-primary" role="status">
+                                <span class="sr-only"></span>
+                            </div>
+                        </div>
+                        </form>  
+                        </div>
+                        <!--<div class="col">
+                            
+                        </div>-->
+                    </div><!-- End of Row -->   
+                </div>
+            </div>
+</div>
 </body>
 </html>
